93e1883cbed8c2c78adc1b5703e6ca577a6e1403
0516khj2004 <0516khj2004@naver.com>
2020-09-06 12:54:41 +0900

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-backgound">
+      <img src={logo} lat="logo"/>
+      <h2>management</h2>
+    </div>
+  );
+}
+
+export default App;
