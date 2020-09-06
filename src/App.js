04f8ff20bcf183be3662e15ad73c71f166ea7f95
0516khj2004 <0516khj2004@naver.com>
2020-09-06 13:25:33 +0900

    customer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customers = [
+  {
+    'id': 1,
+    'img': 'https://placeimg.com/64/64/1',
+    'name': '구현진',
+    'birthday': '960516',
+    'gender': '여자',
+    'job': '직딩',
+  },
+  {
+    'id': 2,
+    'img': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '960516',
+    'gender': '여자',
+    'job': '직딩',
+  },
+  {
+    'id':3,
+    'img': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '960516',
+    'gender': '여자',
+    'job': '직딩',
+  },
+]
 
 function App() {
   return (
-    <div className="gray-backgound">
-      <img src={logo} lat="logo"/>
-      <h2>management</h2>
-    </div>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer 
+              key = {c.id}
+              id = {c.id}
+              img = {c.img}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+            />
+          )
+        })
+      }
+    </div>   
   );
 }
 
